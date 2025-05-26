8ef8b1dca456c84b0f78d5f633f1d319ca6af689
moeka <moemoe.yakiniy.barmi@gmail.com>
2025-05-26 20:14:46 +0900

    forth-laravel

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        // $this->call(CategoriesTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
         $this->call(ContactsTableSeeder::class);
     }
 }
